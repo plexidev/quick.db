9d11ec7500c01020a5d18b7a21967f3d655eaa8a
TrueXPixels <truexpixels@gmail.com>
2017-11-21 16:27:35 -0800

    Initial Support for Arrays

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 Quick.db
 ========
 
-
 <div align="center">
     <p>
         <a href="https://discord.gg/6mcmDu4"><img src="https://discordapp.com/api/guilds/343572980351107077/embed.png" alt="Discord Server" /></a>
@@ -15,20 +14,23 @@ Quick.db
 
 This package is meant to provide an easy way to create and access a sqlite3 database.
 
+
 **Installation**
 ```
 npm install quick.db
 ```
+
 
 **Require Package**
 ```
 var database = require('quick.db')
 ```
 
+## Usage
 
 **Fetch Object**
 
-No need to create a database, it does that automatically.
+No need to create a database OR object, it does everything automatically.
 ```js
 database.fetchObject('ID').then(i => {
     // Code goes here...
@@ -40,12 +42,13 @@ database.fetchObject('ID').then(i => {
 **Update Value**
 
 Value must be an integer. This ADDS to the existing number, if you want to clear it use fetchObject then 
-parseInt(\`-${i.value}\`) of the fetched. If you want to subtract do parseInt(`-${positiveNumber}`) or a negative number.
+parseInt(\`-${i.value}\`) of the fetched. If you want to subtract do parseInt(\`-${positiveNumber}\`) or a negative number.
 
 ```js
 database.updateValue('ID', 'value').then(i => {
 	// Code goes here...
-    console.log(i) // Returns the updated result. Example: If 'value' is 250, it would return { ID: 'ID', value: 250, text: '' }
+    console.log(i) // Returns the updated result. 
+    // Example: If 'value' is 250, it would return { ID: 'ID', value: 250, text: '' }
 })
 ```
 
@@ -56,37 +59,87 @@ This resets the text to what you enter.
 ```js
 database.updateText('ID', 'text').then(i => {
     // Code goes here...
-    console.log(i) // Returns the updated result. Example: If 'text' was 'Hello', it would return { ID: 'ID', value: 0, text: 'Hello' }
+    console.log(i) // Returns the updated result. 
+    // Example: If 'text' was 'Hello', it would return { ID: 'ID', value: 0, text: 'Hello' }
 })
 ```
 
+**Fetch Array**
+```js
+database.fetchArray('ID').then(array => {
+    // Code goes here...
+    console.log(array) // Returns an array. 
+    // If no items in array it will return empty.
+})
+```
 
+**Set Array**
+```js
+database.setArray('ID', ['newArray']).then(array => {
+    // Code goes here...
+    console.log(array) // Returns the new array. 
+    // Example: ['item1', 'item2', 'item3']
+})
+```
 
-**Example** [Another Example w/ Console(image)](https://i.gyazo.com/8ef58aac252fccc46a4c6eceb9505918.png)
+**Example |**  [Another Example w/ Console(image)](https://i.gyazo.com/8ef58aac252fccc46a4c6eceb9505918.png)
 ```js
 // Call Packages
 const db = require('quick.db')
 
-// Fetches values from ID 1, then forwards it to i
+// Fetches values from ID user, then forwards it to i
 db.fetchObject('user').then(i => {
     console.log(i) // { ID: 'user', value: 0, text: '' }
     console.log(i.value) // 0
 })
 
-// Fetches values from ID 1, adds 500 to it, then forwards the updated result to i
+// Fetches values from ID user, adds 500 to it, then forwards the updated result to i
 db.updateValue('user', 500).then(i => {
     console.log(i) // { ID: 'user', value: 500, text: '' }
     console.log(i.value) // 500
 })
 
-// Fetches values from ID 1, adds -250 to it, then forwards the update result to i
+// Fetches values from ID user, adds -250 to it, then forwards the updated result to i
 db.updateValue('user', -250).then(i => {
     console.log(i) // { ID: 'user', value: 250, text: '' }
     console.log(i.value) // 250
 })
 
+// Fetches values from ID user, updates text, then forwards the updated result to i
 db.updateText('user', 'This is a stored string!').then(i => {
     console.log(i) // { ID: 'user', value: 250, text: 'This is a stored string!' }
     console.log(i.text) // 'This is a stored string!'
 })
+
+////////////
+// ARRAYS //
+////////////
+
+// This sets the array associated with the ID 'items'
+db.setArray('items', ['bucket', 'sword', 'shoes']).then(array => {
+    console.log(array) // ['bucket', 'sword', 'shoes']
+})
+
+// This fetches the array associated with the ID 'items'
+db.fetchArray('items').then(array => {
+    console.log(array) // ['bucket', 'sword', 'shoes']
+})
+
+// This pushes a new item to the already existing array
+db.fetchArray('items').then(orgArray => {
+
+    // Push new item to orgArray
+    orgArray.push('pitchfork')
+    
+    db.setArray('items', orgArray).then(newArray => {
+        console.log(newArray) // ['bucket', 'sword', 'shoes', 'pitchfork']
+    })
+    
+})
+
 ```
+
+## Projects Using quick.db
+[Infinitude](https://discordbots.org/bot/346516120322179072) **-** A fully customizable, modular, multi-use discord bot. **-** By *TrueXPixels*
+    
+[Immortal](https://discordbots.org/bot/365689417052192799) **-** A multi-functional discord.js automaton. **-** By *Immortality*
--- index.js
+++ index.js
@@ -8,7 +8,157 @@ const sqlite3 = require('sqlite3').verbose();
 
 module.exports = {
 
-    updateValue: function (ID, increase) {
+    setArray: function(ID, array) {
+        const getInfo = new Promise((resolve, error) => {
+
+            // Check if array is an object
+            if (typeof array !== 'object') {
+                console.log('ARRAY is NOT AN ARRAY');
+                return error('ERROR: ARRAY is NOT AN ARRAY')
+            }
+
+            let db;
+            let response;
+            let arrayKey;
+
+            function createDb() { // Create Database Chain
+                db = new sqlite3.Database('./arrays.sqlite', createTable);
+            }
+
+            function createTable() { // Create table if it doesn't exist
+                db.run("CREATE TABLE IF NOT EXISTS arrays (ID TEXT, array TEXT)", checkForKey());
+            }
+
+            function checkForKey() { // Check if row exists w/ ID
+
+                db.get(`SELECT * FROM arrays WHERE ID = (?)`, 'SECRET_ARRAYKEY_DONOTDELETE', function(err, row) {
+                    if (!row || row.array === 'none') { // Run if row not found...
+                        insertKey()
+                    }
+                    else { // Run if row found...
+                        arrayKey = row.array
+                        checkIfCreated()
+                    }
+                })
+
+            }
+
+            function insertKey() { // Create an empty row w/ ID
+
+                let key = ''
+                let possible = 'ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρςστυφχψωϑϒϖ–—‚""„†‡•…‰‾€Π™←↑→↓↔↵⇐⇑⇒⇓⇔∀∂∃∅∇∈∉∋∏∑−∗√∝∞∠∧∨∩∪∫∴∼≅≈≠≡≤≥⊂⊃⊄⊆⊇⊕⊗⊥⋅⌈⌉⌊⌋〈〉◊♠♣♥♦ŒœŠšŸƒ'
+                for (var i = 0; i < 15; i++) key += possible.charAt(Math.floor(Math.random() * possible.length));
+
+                db.run("INSERT INTO arrays (ID,array) VALUES (?,?)", 'SECRET_ARRAYKEY_DONOTDELETE', key, checkForKey())
+            }
+
+            function checkIfCreated() { // Check if row exists w/ ID
+
+                db.get(`SELECT * FROM arrays WHERE ID = (?)`, ID, function(err, row) {
+                    if (!row) {
+                        insertRows()
+                    }
+                    else {
+                        
+                        let newArray = array.join(arrayKey)
+                        
+                        db.run(`UPDATE arrays SET array = (?) WHERE ID = (?)`, newArray, ID);
+                        db.get(`SELECT * FROM arrays WHERE ID = (?)`, ID, function(err, row) {
+                            response = row.array.split(arrayKey);
+                            returnDb()
+                        })
+                    }
+                })
+
+            }
+
+            function insertRows() { // Create an empty row w/ ID
+                db.run("INSERT INTO arrays (ID,array) VALUES (?,?)", ID, "", checkIfCreated)
+            }
+
+            function returnDb() { // Returns Row
+                db.close();
+                return resolve(response)
+            }
+
+            createDb()
+
+        });
+
+        return getInfo;
+
+    },
+
+    fetchArray: function(ID) {
+        const getInfo = new Promise((resolve) => {
+
+            let db;
+            let response;
+            let arrayKey;
+
+            function createDb() { // Create Database Chain
+                db = new sqlite3.Database('./arrays.sqlite', createTable)
+            }
+
+            function createTable() { // Create table if it doesn't exist
+                db.run("CREATE TABLE IF NOT EXISTS arrays (ID TEXT, array TEXT)", checkForKey)
+            }
+
+            function checkForKey() { // Check if row exists w/ ID
+
+                db.get(`SELECT * FROM arrays WHERE ID = (?)`, 'SECRET_ARRAYKEY_DONOTDELETE', function(err, row) {
+                    if (!row || row.array === 'none') { // Run if row not found...
+                        insertKey()
+                    }
+                    else { // Run if row found...
+                        arrayKey = row.array
+                        checkIfCreated()
+                    }
+                })
+
+            }
+
+            function insertKey() { // Create an empty row w/ ID
+
+                let key = ''
+                let possible = 'ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩαβγδεζηθικλμνξοπρςστυφχψωϑϒϖ–—‚""„†‡•…‰‾€Π™←↑→↓↔↵⇐⇑⇒⇓⇔∀∂∃∅∇∈∉∋∏∑−∗√∝∞∠∧∨∩∪∫∴∼≅≈≠≡≤≥⊂⊃⊄⊆⊇⊕⊗⊥⋅⌈⌉⌊⌋〈〉◊♠♣♥♦ŒœŠšŸƒ'
+                for (var i = 0; i < 15; i++) key += possible.charAt(Math.floor(Math.random() * possible.length));
+
+                db.run("INSERT INTO arrays (ID,array) VALUES (?,?)", 'SECRET_ARRAYKEY_DONOTDELETE', key, checkForKey())
+            }
+
+            function checkIfCreated() { // Check if row exists w/ ID
+
+                db.get(`SELECT * FROM arrays WHERE ID = (?)`, ID, function(err, row) {
+                    if (!row) { // Run if row not found...
+                        insertRows()
+                    }
+                    else { // Run if row found...
+                        response = row.array.split(arrayKey);
+                        returnDb()
+                    }
+                })
+
+            }
+
+            function insertRows() { // Create an empty row w/ ID
+                db.run("INSERT INTO arrays (ID,array) VALUES (?,?)", ID, "", checkIfCreated)
+            }
+
+            function returnDb() { // Return Row
+                db.close();
+                return resolve(response)
+            }
+
+            createDb()
+
+        });
+
+        return getInfo
+
+    },
+
+    updateValue: function(ID, increase) {
 
         const getInfo = new Promise((resolve, error) => {
 
@@ -34,13 +184,14 @@ module.exports = {
             }
 
             function checkIfCreated() { // Check if row exists w/ ID
-                db.get(`SELECT * FROM database WHERE ID = (?)`, ID, function (err, row) {
+                db.get(`SELECT * FROM database WHERE ID = (?)`, ID, function(err, row) {
 
                     if (!row) { // Run if it doesn't exist...
                         insertRows()
-                    } else { // Run if it does exist...
+                    }
+                    else { // Run if it does exist...
                         db.run(`UPDATE database SET value = (?) WHERE ID = (?)`, row.value + increase, ID);
-                        db.get(`SELECT * FROM database WHERE ID = (?)`, ID, function (err, row) {
+                        db.get(`SELECT * FROM database WHERE ID = (?)`, ID, function(err, row) {
                             response = row;
                             returnDb()
                         })
@@ -66,7 +217,7 @@ module.exports = {
 
     },
 
-    fetchObject: function (ID) {
+    fetchObject: function(ID) {
         const getInfo = new Promise((resolve) => {
 
             let db;
@@ -82,10 +233,11 @@ module.exports = {
 
             function checkIfCreated() { // Check if row exists w/ ID
 
-                db.get(`SELECT * FROM database WHERE ID = (?)`, ID, function (err, row) {
+                db.get(`SELECT * FROM database WHERE ID = (?)`, ID, function(err, row) {
                     if (!row) { // Run if row not found...
                         insertRows()
-                    } else { // Run if row found...
+                    }
+                    else { // Run if row found...
                         response = row;
                         returnDb()
                     }
@@ -110,7 +262,7 @@ module.exports = {
 
     },
 
-    updateText: function (ID, text) {
+    updateText: function(ID, text) {
         const getInfo = new Promise((resolve, error) => {
 
             // Check if text is a string
@@ -132,12 +284,13 @@ module.exports = {
 
             function checkIfCreated() { // Check if row exists w/ ID
 
-                db.get(`SELECT * FROM database WHERE ID = (?)`, ID, function (err, row) {
+                db.get(`SELECT * FROM database WHERE ID = (?)`, ID, function(err, row) {
                     if (!row) {
                         insertRows()
-                    } else {
+                    }
+                    else {
                         db.run(`UPDATE database SET text = (?) WHERE ID = (?)`, text, ID);
-                        db.get(`SELECT * FROM database WHERE ID = (?)`, ID, function (err, row) {
+                        db.get(`SELECT * FROM database WHERE ID = (?)`, ID, function(err, row) {
                             response = row;
                             returnDb()
                         })
@@ -163,11 +316,11 @@ module.exports = {
 
     },
 
-    fetchValue: function (ID) { // THIS IS DEPRECATED. USE fetchObject(ID).then(i => {})
-    
+    fetchValue: function(ID) {
+
         console.log("\nQUICK.DB WARNING: 'fetchValue(ID).then(i => {})' is deprecated. Please use 'fetchObject(ID).then(i => {})")
         console.log("QUICK.DB WARNING: 'fetchValue(ID).then(i => {})' is deprecated. Please use 'fetchObject(ID).then(i => {})\n")
-                    
+
         const getInfo = new Promise((resolve) => {
 
             let db;
@@ -183,10 +336,11 @@ module.exports = {
 
             function checkIfCreated() { // Check if row exists w/ ID
 
-                db.get(`SELECT * FROM database WHERE ID = (?)`, ID, function (err, row) {
+                db.get(`SELECT * FROM database WHERE ID = (?)`, ID, function(err, row) {
                     if (!row) { // Run if row not found...
                         insertRows()
-                    } else { // Run if row found...
+                    }
+                    else { // Run if row found...
                         response = row;
                         returnDb()
                     }
@@ -209,7 +363,82 @@ module.exports = {
 
         return getInfo
 
-    },
+    }
+    /* Disabled
+        fetchTop: function(column, amount) { // Column == value, or text
 
+            const getInfo = new Promise((resolve) => {
+
+                // Return Statements
+                if (column.toUpperCase() !== 'VALUE'.toUpperCase() || column.toUpperCase() !== 'TEXT'.toUpperCase()) {
+                    console.log('Quick.db Error w/ fetchTop: Column can only be (VALUE, TEXT).')
+                    return error('Quick.db Error w/ fetchTop: Column can only be (VALUE, TEXT).')
+                }
+
+                if (isNaN(amount)) {
+                    console.log('Quick.db Error w/ fetchTop: amount is not an integer.')
+                    return error('Quick.db Error w/ fetchTop: amount is not an integer.')
+                }
+
+
+                let db;
+                let response;
+
+                function createDb() { // Create Database Chain
+                    db = new sqlite3.Database('./database.sqlite', createTable)
+                }
+
+                function createTable() { // Create table if it doesn't exist
+                    db.run("CREATE TABLE IF NOT EXISTS database (ID TEXT, value INTEGER, text TEXT)", checkIfCreated)
+                }
+
+                function checkIfCreated() { // Check if row exists w/ ID
+
+                    db.get(`SELECT * FROM database ORDER BY (?) DESC LIMIT (?)`, column, amount, function(err, rows) {
+                        if (!rows) { // Run if row not found...
+                            insertRows()
+                        }
+                        else { // Run if row found...
+
+                            function asyncFunction(item, callback) {
+                                setTimeout(() => {
+                                    console.log(`Parsed: ${item}`)
+                                    callback();
+                                }, 100)
+                            }
+
+                            let requests = rows.forEach(item => {
+                                return new Promise((resolve) => {
+                                    asyncFunction(item, resolve);
+                                });
+                            })
+
+                            requests.then(() => {
+                                response = rows
+                                returnDb()
+                            })
+
+                        }
+                    })
+
+                }
+
+                function insertRows() { // Create an empty row w/ ID
+                    db.run("INSERT INTO database (ID,value,text) VALUES (?,?,?)", ID, 0, "", checkIfCreated)
+                }
+
+                function returnDb() { // Return Row
+                    db.close();
+                    return resolve(response)
+                }
+
+                createDb()
+
+            });
+
+            return getInfo
+
+        }
+    */
 
 };

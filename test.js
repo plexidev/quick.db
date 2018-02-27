6e2d2d54b82d12ee0119aafdc8d190f395d90d22
Glitch (quick-db) <none>
2018-02-27 18:59:28 +0000

    🎮💵 Quick.db v5.2.1

--- test.js
+++ test.js
@@ -1,15 +1,12 @@
 const db = require('./index.js');
  
-let number = 100;
+let ID = 'unique'
 
-db.set('uniqueID', number)
-db.fetch('uniqueID').then(i => console.log(i))
+/*db.add(ID, 20)
+db.fetch(ID).then(i => console.log(i))
 
-db.add('uniqueID', 'cheese')
-db.fetch('uniqueID').then(i => console.log(i))
-
-db.subtract('uniqueID', 1000.10)
-db.fetch('uniqueID').then(i => {
+db.subtract(ID, 10)
+db.fetch(ID).then(i => {
   console.log(i)
   console.log(typeof i)
-})
+})*/

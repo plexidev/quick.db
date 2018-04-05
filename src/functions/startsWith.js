3c5ff8d2b1450088b14d3735d824e07ceaf48e21
Glitch (quickdb-latest) <none>
2018-04-05 17:46:16 +0000

    Code Formatting

--- src/functions/startsWith.js
+++ src/functions/startsWith.js
@@ -1,44 +1,49 @@
 const sort = require('array-sort');
 
 module.exports = function(startsWith, options, db) {
-    const getInfo = new Promise((resolve, error) => {
-        
-        if (typeof startsWith !== 'string') return console.log('ERROR: db.startsWith(text) | text is not a string.')
-        
-        if (options) {
-          options = {
-            sort: options.sort || undefined 
-          }
-        }
-      
-        let response = [];
+  const getInfo = new Promise((resolve, error) => {
 
-        function createDb() {
-            db.prepare("CREATE TABLE IF NOT EXISTS json (ID TEXT, json TEXT)").run();
-            fetchAll();
-        }
+    if (typeof startsWith !== 'string') return console.log('ERROR: db.startsWith(text) | text is not a string.')
 
-        function fetchAll() {
-            let resp = db.prepare(`SELECT * FROM json`).all();
-            resp.forEach(function(entry){
-              if (entry.ID === null) return;
-              if (entry.ID === 'WEBVIEW_ACTIVE_SOCKETS') return;
-              if (!entry.ID.startsWith(startsWith)) return;
-              response.push({ ID: entry.ID, data: JSON.parse(entry.json) })
-            })
-            if (options && typeof options.sort === 'string') {
-              if (options.sort.startsWith('.')) options.sort = options.sort.slice(1)
-              response = sort(response, options.sort, { reverse: true })
-            }
-            returnDb();
-        }
+    if (options) {
+      options = {
+        sort: options.sort || undefined
+      }
+    }
 
-        function returnDb() {
-            return resolve(response);
-        }
+    let response = [];
 
-        createDb();
+    function createDb() {
+      db.prepare("CREATE TABLE IF NOT EXISTS json (ID TEXT, json TEXT)").run();
+      fetchAll();
+    }
 
-    });
-    return getInfo;
+    function fetchAll() {
+      let resp = db.prepare(`SELECT * FROM json`).all();
+      resp.forEach(function(entry) {
+        if (entry.ID === null) return;
+        if (entry.ID === 'WEBVIEW_ACTIVE_SOCKETS') return;
+        if (!entry.ID.startsWith(startsWith)) return;
+        response.push({
+          ID: entry.ID,
+          data: JSON.parse(entry.json)
+        })
+      })
+      if (options && typeof options.sort === 'string') {
+        if (options.sort.startsWith('.')) options.sort = options.sort.slice(1)
+        response = sort(response, options.sort, {
+          reverse: true
+        })
+      }
+      returnDb();
+    }
+
+    function returnDb() {
+      return resolve(response);
+    }
+
+    createDb();
+
+  });
+  return getInfo;
 }

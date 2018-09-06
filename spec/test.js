90d7f5a538dd833ffeb0c954ac9da8a5261a6201
Glitch (quickdb2) <none>
2018-09-06 21:19:09 +0000

    Update v7.0.0 beta 11

--- spec/test.js
+++ spec/test.js
@@ -1,24 +1,10 @@
 // Require Package
-const db = require('../index.js'); // Equivalent to require('quick.db')
+const db = require('../index.js'); 
+db.set('test3', [ 2 ]);
+console.log(db.push('test3', 5))
+db.set("test", {ree: {reee: 5}});
 
-// Setting an object in the database:
-console.log(db.set('userInfo', { difficulty: 'Easy' }))
-// -> { difficulty: 'Easy' }
- 
-// Pushing an element to an array (that doesn't exist yet) in an object:
-console.log(db.push('userInfo.items', 'Sword'))
-// -> { difficulty: 'Easy', items: ['Sword'] }
- 
-// Adding to a number (that doesn't exist yet) in an object:
-console.log(db.add('userInfo.balance', 500))
-// -> { difficulty: 'Easy', items: ['Sword'], balance: 500 }
- 
-// Repeating previous examples:
-console.log(db.push('userInfo.items', 'Watch'))
-// -> { difficulty: 'Easy', items: ['Sword', 'Watch'], balance: 500 }
-console.log(db.add('userInfo.balance', 500))
-// -> { difficulty: 'Easy', items: ['Sword', 'Watch'], balance: 1000 }
- 
-// Fetching individual properties
-console.log(db.get('userInfo.balance')) // -> 1000
-console.log(db.get('userInfo.items')) // ['Sword', 'Watch']
+console.log(db.fetch("test"))
+console.log(db.fetch("test.ree"))
+console.log(db.fetch("test.ree.reee"))
+console.log(db.add("test.ree.reee", 50).ree.reee)

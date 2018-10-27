b0853b502ff9fc696526236e9d1ac9e698362782
Zoro- <swftapocalypse1@gmail.com>
2018-10-27 12:12:08 -0400

    Typings

---
+++ typings/index.d.ts
@@ -0,0 +1,35 @@
+declare module 'quick.db' {
+
+    function fetch(key: string, ops?: object): any;
+    function get(key: string, ops?: object): any;
+
+    function set(key: string, value: string|object|Array<any>, ops?: object): any;
+
+    function add(key: string, value: number, ops?: object): any;
+    function subtract(key: string, value: number, ops?: object): any;
+
+    function push(key: string, value: string|object|Array<any>): Array<any>;
+
+    function has(key: string, ops?: object): boolean;
+    function includes(key: string, ops?: object): boolean;
+
+    function all(ops?: object): boolean;
+    function fetchAll(ops?: object): boolean;
+
+    function del(key: string, ops?:object): boolean
+
+    export {
+        fetch,
+        get,
+        set,
+        add,
+        subtract,
+        push,
+        has,
+        includes,
+        all,
+        fetchall,
+        del as delete
+    }
+
+}

4d241ce145f32efaa39740e3534f33c861126820
ndr3w221 <kalaz.bb@gmail.com>
2018-12-02 19:01:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-// Thank you for using quick.db!
-// This is an open-sourced better-sqlite3 wrapped designed to be easy to setup & utilize.
-
-// We have an official verified Discord!
-// https://discord.gg/plexidev
+/* Thank you for using quick.db!
+ * This is an open-sourced better-sqlite3 wrapped designed to be easy to setup & utilize.
+ *
+ * We have an official verified Discord!
+ * https://discord.gg/plexidev
+ */
 
 module.exports = require('./bin/handler.js')
 
-// Looking for tutorials?
-// We have some coming soon @ youtube.com/c/TrueXPixels, subscribe to stay notified!
-
-// Documentation:
-// https://quickdb.js.org
+/* Looking for tutorials?
+ * We have some coming soon youtube.com/c/TrueXPixels, subscribe to stay notified!
+ *
+ * Documentation:
+ * https://quickdb.js.org
+ */

32e115e709ed4789712bb330ed0381a12cae9fa8
Snowflake <devsnowflake107@gmail.com>
2020-10-02 09:03:18 +0545

    typings

--- index.d.ts
+++ index.d.ts
@@ -1,78 +1,115 @@
 /**
- * Definitions by: Zoro <admin@zoro.tech>
+ * quick.db definitions
  */
 
 declare module 'quick.db' {
+    interface Options {
+        target?: string | null;
+        table?: string;
+    }
+
+    type ValueData = string | object | number | any[];
 
     /**
-     * This function fetches data from a key in the database. (alias: .get())
-     * @param {key} input any string as a key. Also allows for dot notation following the key.
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {data} the data requested.
+     * Package version. Community requested feature.
+     * ```
+     * console.log(require('quick.db').version);
+     * ```
      */
-    function fetch(key: string, ops?: object): any;
+    const version: string;
+        
+    /**
+     * This function fetches data from a key in the database.
+     * @param key Any string as a key. Also allows for dot notation following the key.
+     * @param ops Any options to be added to the request.
+     */
+    function fetch(key: string, ops?: Options): any;
+
+    /**
+     * This function fetches data from a key in the database.
+     * @param key Any string as a key. Also allows for dot notation following the key.
+     * @param ops Any options to be added to the request.
+     */
     function get(key: string, ops?: object): any;
-
-
 
     /**
      * This function sets new data based on a key in the database. 
-     * @param {key} input any string as a key. Also allows for dot notation following the key.
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {data} the updated data.
+     * @param key Any string as a key. Also allows for dot notation following the key.
+     * @param value Value to set.
+     * @param ops Any options to be added to the request.
      */
-    function set(key: string, value: string|object|number|Array<any>, ops?: object): any;
+    function set(key: string, value: ValueData, ops?: Options): any;
 
     /**
      * This function adds a number to a key in the database. (If no existing number, it will add to 0)
-     * @param {key} input any string as a key. Also allows for dot notation following the key.
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {data} the updated data.
+     * @param key Any string as a key. Also allows for dot notation following the key.
+     * @param value Any numerical value.
+     * @param ops Any options to be added to the request.
      */
-    function add(key: string, value: number, ops?: object): any;
+    function add(key: string, value: number, ops?: Options): any;
 
     /**
      * This function subtracts a number to a key in the database. (If no existing number, it will subtract from 0)
-     * @param {key} input any string as a key. Also allows for dot notation following the key.
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {data} the updated data.
+     * @param key Any string as a key. Also allows for dot notation following the key.
+     * @param value Any numerical value.
+     * @param ops Any options to be added to the request.
      */
-    function subtract(key: string, value: number, ops?: object): any;
+    function subtract(key: string, value: number, ops?: Options): any;
 
     /**
      * This function will push into an array in the database based on the key. (If no existing array, it will create one)
-     * @param {key} input any string as a key. Also allows for dot notation following the key.
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {data} the updated data.
+     * @param key Any string as a key. Also allows for dot notation following the key.
+     * @param value Any data to push.
+     * @param ops Any options to be added to the request.
      */
-    function push(key: string, value: string|object|number|Array<any>): Array<any>;
+    function push(key: string, value: ValueData, ops?: Options): any[];
 
     /**
      * This function returns a boolean indicating whether an element with the specified key exists or not.
-     * @param {key} input any string as a key. Also allows for dot notation following the key, this will return if the prop exists or not.
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {boolean} if it exists.
+     * @param key Any string as a key. Also allows for dot notation following the key, this will return if the prop exists or not.
+     * @param ops Any options to be added to the request.
      */
-    function has(key: string, ops?: object): boolean;
+    function has(key: string, ops?: Options): boolean;
+
+    /**
+     * This function returns a boolean indicating whether an element with the specified key exists or not.
+     * @param key Any string as a key. Also allows for dot notation following the key, this will return if the prop exists or not.
+     * @param ops Any options to be added to the request.
+     */
     function includes(key: string, ops?: object): boolean;
 
     /**
      * This function fetches the entire active table
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {boolean} if it exists.
+     * @param ops Any options to be added to the request.
      */
-    function all(ops?: object): { ID: any; data: any; }[];
-    function fetchAll(ops?: object): { ID: any; data: any; }[];
-    
-    function table(tableName: string, options: any): any;
+    function all(ops?: Options): { ID: string; data: any; }[];
+
+    /**
+     * This function fetches the entire active table
+     * @param ops Any options to be added to the request.
+     */
+    function fetchAll(ops?: Options): { ID: string; data: any; }[];
     
     /**
      * This function will delete an object (or property) in the database.
-     * @param {key} input any string as a key. Also allows for dot notation following the key, this will delete the prop in the object.
-     * @param {options} [input={ target: null }] Any options to be added to the request.
-     * @returns {boolean} if it was a success or not.
+     * @param key Any string as a key. Also allows for dot notation following the key, this will delete the prop in the object.
+     * @param ops Any options to be added to the request.
      */
-    function del(key: string, ops?:object): boolean;
+    function del(key: string, ops?: Options): boolean;
+
+    /**
+     * Used to get the type of the value.
+     * @param key Any string as a key. Also allows for dot notation following the key, this will delete the prop in the object.
+     * @param ops Any options to be added to the request.
+     */
+    function type(key: string, ops?: Options): "string" | "number" | "bigint" | "boolean" | "symbol" | "undefined" | "object" | "function";
+
+    /**
+     * Using 'new' on this function creates a new instance of a table.
+     * @param tableName Any string as the name of the table.
+     * @param options Options
+     */
+    function table(tableName: string, options?: object): any;
 
     export {
         fetch,
@@ -85,7 +122,10 @@ declare module 'quick.db' {
         includes,
         all,
         fetchAll,
-        del as delete
+        table,
+        del as delete,
+        type,
+        version
     }
 
 }

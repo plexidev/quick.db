f09e0f5ee22e75a4d790e22a0c9347941d3fdae6
David Neidhart <david.neidhart@gmx.net>
2022-07-10 17:18:39 +0200

    Add startsWith function

--- src/index.ts
+++ src/index.ts
@@ -227,6 +227,22 @@ export class QuickDB {
         return this.set(key, currentArr);
     }
 
+    async startsWith<T>(
+        query: string,
+        key: string = ''
+    ): Promise<{ id: string; value: any }[]> {
+
+        if (typeof query != "string")
+            throw new Error("First argument (query) needs to be a string");
+        if (typeof key != "string")
+            throw new Error("Second argument (key) needs to be a string");
+
+        // Get either the whole db or the rows from the provided key
+        // -> Filter the result if the id starts with the provided query
+        // -> Return the filtered result
+        return ((key === '' ? await this.all() : await this.get(key) ?? []) as { id: string; value: any }[]).filter((v) => v.id.startsWith(query));
+    }
+
     table(table: string): QuickDB {
         if (typeof table != "string")
             throw new Error("First argument (table) needs to be a string");

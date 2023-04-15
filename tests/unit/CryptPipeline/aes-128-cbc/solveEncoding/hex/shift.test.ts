70dfe94ff4684f10d27b72bf0a3bddc6941ed0dd
DarkenLM <thebigergameroficial@gmail.com>
2023-04-15 21:05:17 +0100

    test(crypt_pipeline): added tests for CryptPipeline

---
+++ tests/unit/CryptPipeline/aes-128-cbc/solveEncoding/hex/shift.test.ts
@@ -0,0 +1,69 @@
+import { PipeLiner, QuickDB } from "../../../../../../src";
+import { EntryGenerator } from "../../../../../generators/EntryGenerator";
+import { faker } from "@faker-js/faker";
+import { SqliteDriverMock } from "../../../../../mocks/SqliteDriver";
+import { CryptPipeline } from "../../../../../../src/pipeline/crypto/crypt";
+import { randomBytes } from "crypto";
+
+const SQLiteInstance = new SqliteDriverMock("test.sqlite");
+const key = randomBytes(8).toString('hex');
+const crypt = new CryptPipeline({
+	algorithm: "aes-128-cbc",
+	key: key,
+	encoding: "hex",
+	solveEncoding: true
+});
+
+const pipeline = new PipeLiner(SQLiteInstance, crypt);
+const db = new QuickDB({
+    driver: pipeline,
+});
+db.init();
+
+describe("shift", () => {
+    afterEach(async () => {
+        SqliteDriverMock.mockClear();
+        await db.deleteAll();
+    });
+
+    describe("no initial data", () => {
+        afterEach(async () => {
+            SqliteDriverMock.mockClear();
+            await db.deleteAll();
+        });
+
+        it("should shift entry no data return undefined", async () => {
+            const entry = EntryGenerator.generateEntry<number>(
+                faker.datatype.number
+            );
+            const returned = await db.shift(entry.id);
+            expect(returned).toEqual(undefined);
+        });
+    });
+
+    describe("with initial data", () => {
+        beforeEach(async () => {
+            await db.set("test", [5, 6]);
+            await db.set("object", { test: [10, 19] });
+        });
+
+        afterEach(async () => {
+            SqliteDriverMock.mockClear();
+            await db.deleteAll();
+        });
+
+        it("should shift entry", async () => {
+            const returned = await db.shift("test");
+            const result = await db.get("test");
+            expect(result).toEqual([6]);
+            expect(returned).toEqual(5);
+        });
+
+        it("should shift from object property", async () => {
+            const returned = await db.shift("object.test");
+            const result = await db.get("object.test");
+            expect(result).toEqual([19]);
+            expect(returned).toEqual(10);
+        });
+    });
+});
---
+++ tests/unit/CryptPipeline/aes-192-cbc/NotSolveEncoding/base64/startsWith.test.ts
@@ -0,0 +1,67 @@
+import { PipeLiner, QuickDB } from "../../../../../../src";
+import { EntryGenerator } from "../../../../../generators/EntryGenerator";
+import { faker } from "@faker-js/faker";
+import { SqliteDriverMock } from "../../../../../mocks/SqliteDriver";
+import { CryptPipeline } from "../../../../../../src/pipeline/crypto/crypt";
+import { randomBytes } from "crypto";
+
+const SQLiteInstance = new SqliteDriverMock("test.sqlite");
+const key = randomBytes(12).toString('hex');
+const crypt = new CryptPipeline({
+	algorithm: "aes-192-cbc",
+	key: key,
+	encoding: "base64",
+	solveEncoding: false
+});
+
+const pipeline = new PipeLiner(SQLiteInstance, crypt);
+const db = new QuickDB({
+    driver: pipeline,
+});
+db.init();
+
+describe("startsWith", () => {
+    afterEach(async () => {
+        SqliteDriverMock.mockClear();
+        await db.deleteAll();
+    });
+
+    describe("no initial data", () => {
+        afterEach(async () => {
+            SqliteDriverMock.mockClear();
+            await db.deleteAll();
+        });
+
+        it("should return empty array", async () => {
+            const entry = EntryGenerator.generateEntry<number>(
+                faker.datatype.number
+            );
+            const returned = await db.startsWith(entry.id);
+            expect(returned).toEqual([]);
+        });
+    });
+
+    describe("with initial data", () => {
+        beforeEach(async () => {
+            await db.set("test_1", true);
+            await db.set("test_2", true);
+            await db.set("test_3", true);
+            await db.set("nope_1", true);
+        });
+
+        afterEach(async () => {
+            SqliteDriverMock.mockClear();
+            await db.deleteAll();
+        });
+
+        it("should returnall tests", async () => {
+            const returned = await db.startsWith("test");
+            expect(returned.length).toEqual(3);
+            for (let i = 0; i < returned.length; i++) {
+                const el = returned[i];
+                expect(el.id).toEqual(`test_${i + 1}`);
+                expect(el.value).toEqual(true);
+            }
+        });
+    });
+});
